3a8d0c7fee82b5a3f461025ecaaf7da9791eba16
Andrew Del Prete <andrew@musicbed.com>
2016-08-26 20:17:19 -0800

    Minify and Autoprefix nb.css via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp')
+    autoprefixer = require('autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
     file = require('gulp-file'),
+    gulpPostcss = require('gulp-postcss'),
     postcss = require('postcss'),
     postcssJs = require('postcss-js'),
-    sass = require('gulp-sass'),
-    stream = require('vinyl-source-stream')
+    sass = require('gulp-sass')
 
 var nb = require('./build')
 
@@ -16,9 +18,14 @@ gulp.task('css', function() {
 
     return postcss()
         .process(cssObj, { parser: postcssJs })
-        .then(result => {
+        .then(function(result) {
             file('nb.css', result.css, { src: true })
                 .pipe(sass())
+                .pipe(gulpPostcss([ autoprefixer ]))
+                .pipe(cleanCSS({ debug: true }, function(details) {
+                    console.log('Original: ' + details.stats.originalSize)
+                    console.log('Minified: ' + details.stats.minifiedSize)
+                }))
                 .pipe(gulp.dest('build'))
         })
 })
