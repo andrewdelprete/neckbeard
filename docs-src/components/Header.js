a4d767177bebd8d2a276d98f2813f506018eaaeb
Colin Howeth <cbhoweth@gmail.com>
2016-09-02 14:33:18 -0500

    added different taglines

--- docs-src/components/Header.js
+++ docs-src/components/Header.js
@@ -4,13 +4,22 @@ import { Router, Route, Link } from 'react-router'
 /**
  * Header
  */
-const Header = () => (
-    <div className={ nb("bg1 flex aic acc wrap") }>
-        <div className={ nb("tc5 fwthin uppercase pv2 mr2 ft6 sans ls3") }>
-            <Link to="/" className={ nb("xtd")}>Neckbeard</Link>
-        </div>
-        <div className={ nb("tcb sm-dn md-db fwsemibold ft3 ls1 sans") }>Your kids will thank us</div>
-    </div>
-)
+export default class Header extends React.Component {
+    componentDidMount() {
+        const taglines = ['Your kids will thank us', 'The Future', 'Pushing it to the next level for future generations', 'Not your mothers CSS']
+        const tagline = document.getElementById('tagline')
 
-export default Header
+        tagline.innerHTML = taglines[Math.floor(Math.random()*taglines.length)]
+    }
+
+    render() {
+        return (
+            <div className={ nb("bg1 flex aic acc wrap") }>
+                <div className={ nb("tc5 fwthin uppercase pv2 mr2 ft6 sans ls3") }>
+                    <Link to="/" className={ nb("xtd")}>Neckbeard</Link>
+                </div>
+                <div className={ nb("tcb sm-dn md-db fwsemibold ft3 ls1 sans") } id="tagline">Default</div>
+            </div>
+        )
+    }
+}
