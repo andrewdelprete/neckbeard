ee91ab44829a9ae0aec473ea74790f417596484d
Andrew Del Prete <andrew@musicbed.com>
2016-09-03 17:54:39 -0800

    Minor doc updates to hopefully page Overview page more readable

--- docs-src/pages/Overview.js
+++ docs-src/pages/Overview.js
@@ -33,8 +33,8 @@ const Installation = () => (
         <H1>Overview</H1>
         <H2>Why Neckbeard?</H2>
         <Copy>
-            Neckbeard is an Atomic CSS in JS framework that leverages the composibility nature of helper (utility) classes and the awesome power of <a href="https://github.com/Khan/aphrodite" className={ nb("tc5 xtd h-tc1") }>Aphrodite</a> to rapidly build components.
-            Neckbeard is created by <a href="https://www.twitter.com/andrewdelprete" className={ nb("tc5 xtd h-tc1") }>@andrewdelprete</a> and <a href="https://www.twitter.com/colinhoweth" className={ nb("tc5 xtd h-tc1") }>@colinhoweth</a> and highly inspired by <a href="https://www.buildwithbeard.com" className={ nb("tc5 xtd h-tc1") }>Beard</a>, built by <a href="https://www.twitter.com/davidhemphill" className={ nb("tc5 xtd h-tc1") }>@davidhemphill</a> and uses much of the same syntax as Beard.
+            Neckbeard is an Atomic CSS in Javascript framework that leverages the composibility nature of helper (utility) classes and the awesome power of <a href="https://github.com/Khan/aphrodite" className={ nb("tc5 xtd h-tc1") }>Aphrodite</a> to rapidly build components.
+            Neckbeard is created by <a href="https://www.twitter.com/andrewdelprete" className={ nb("tc5 xtd h-tc1") }>@andrewdelprete</a> and <a href="https://www.twitter.com/colinhoweth" className={ nb("tc5 xtd h-tc1") }>@colinhoweth</a> and highly inspired by <a href="https://www.buildwithbeard.com" className={ nb("tc5 xtd h-tc1") }>Beard CSS</a> and mimics much of the same syntax as Beard.
             The main difference is Neckbeard is built with Javascript and Beard is built with SCSS. Using Javascript to build a CSS framework allows us to do some
             fantastic things.
         </Copy>
@@ -42,24 +42,29 @@ const Installation = () => (
         <Copy>
             A Helper Class is a very flat and simple method for applying aesthetic styles to HTML elements. These classes can be used in numerous contexts and are optimal for
             composing small to large components or sprinkling throughout your site or application. Neckbeard provides a Helper Class for just about everything you can think
-            of and is extensible per project if custom classes are necessary. An example of a Helper Class to add spacing looks like <Code>.pv1</Code> which translates
+            of and is extensible per project if custom classes are necessary.
+        </Copy>
+        <Copy>
+            An example of a Helper Class to add spacing looks like <Code>.pv1</Code> which translates
             to <Code>padding-top: 1rem; padding-bottom: 1rem;</Code> and the classes making up these code blocks consist of <Code>.ft3.brdr1.br2.bcg10.bgg05.pv05.ph1</Code>.
             In fact this entire site is made up of such helpers.
         </Copy>
         <H3>CSS in Javascript</H3>
         <Copy>
-            CSS in Javascript is a fairly new practice in which we write our CSS in Javascript as an object and load styles on the page dynamically. Because CSS syntax is fairly JSONish to begin with, using Javascript
-            just makes sense. Javascript gives us the power of a real programming language to generate, extend, and compose CSS classes. Whereas something like SCSS or LESS can feel consiberably more limiting. We use React in our examples, however Neckbeard is
-            framework agnostic and can be used anywhere Javascript is used to add CSS classes.
+            CSS in Javascript is a fairly new practice in which we write CSS in Javascript as an object and load styles on the page dynamically. Because the CSS syntax is fairly JSONish to begin with, using Javascript
+            just makes sense. Javascript gives us the power of a real programming language to generate, extend, and compose CSS classes. Tools like SCSS and LESS are great but feel limiting at times.
+        </Copy>
+        <Copy>
+            We use ReactJS in our examples, however Neckbeard is framework agnostic and can be used anywhere Javascript is used to add CSS classes.
         </Copy>
         <Copy>
             Neckbeard wraps the library <a href="https://github.com/Khan/aphrodite" className={ nb("tc5 xtd h-tc1") }>Aphrodite</a> under the hood. Aphrodite is used to only apply the styles you're actually using to the page.
         </Copy>
         <H3>The Magical Unicorn 🦄</H3>
         <Copy>
-            When we combine both the Helper Classes and CSS in Javascript concepts together something magical happens.
-            <div className={ nb("ph1 pt1 tcg40 sm-mb1 md-xmb") }>- It works great with component based libraries / frameworks like React because you can couple style + component in the same file together forever in harmony.</div>
-            <div className={ nb("ph1 tcg40 sm-mb1 md-xmb") }>- In the case with something like React, your component name IS all you are concerned with.</div>
+            When we combine the Helper Classes and the CSS in Javascript concepts together something magical happen:
+            <div className={ nb("ph1 pt1 tcg40 sm-mb1 md-xmb") }>- It works great with component based libraries / frameworks like ReactJS because you can couple style + component in the same file together forever in harmony.</div>
+            <div className={ nb("ph1 tcg40 sm-mb1 md-xmb") }>- In the case with something like ReactJS, your component name IS all you are concerned with.</div>
             <div className={ nb("ph1 tcg40 sm-mb1 md-xmb") }>- No more global CSS scope.</div>
             <div className={ nb("ph1 tcg40 sm-mb1 md-xmb") }>- No longer worry about naming CSS selectors.</div>
             <div className={ nb("ph1 tcg40 sm-mb1 md-xmb") }>- No namespacing to try and establish block level elements.</div>
@@ -67,7 +72,7 @@ const Installation = () => (
         </Copy>
 
         <div className={ nb("mb1") }>
-            <H3>Example Usage in React</H3>
+            <H3>Example in ReactJS</H3>
         </div>
 
         <div className={ nb("lh4 mb2") }>
